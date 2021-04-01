8e481a28296814310cbbecf65754fde23cea87ca
Grimm33 <yandrick.alpha.grima@gmail.com>
2021-04-01 13:51:58 +0200

    Dependency Injection using Spring Framework
    adding @Service will tell Spring to add this class to services
    
    @Autowire is needed only when injecting without constructor
    
    Lesson 35

--- src/main/java/ccbill/training/sfgdi/controllers/SetterInjectedController.java
+++ src/main/java/ccbill/training/sfgdi/controllers/SetterInjectedController.java
@@ -1,10 +1,14 @@
 package ccbill.training.sfgdi.controllers;
 
 import ccbill.training.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SetterInjectedController {
     private GreetingService greetingService;
 
+    @Autowired
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
